39d484537ab29efa7e958a046853b2de0deeadc1
Alexis <alexis.contreras01@alumnos.ucn.cl>
2024-06-30 10:41:38 -0400

    feat: Add GradeController, Grade model, and grades table migration

---
+++ database/migrations/2024_06_30_033633_create_grades_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGradesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('grades', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('subject_name');
+            $table->string('subject_code');
+            $table->decimal('grade', 3, 1)->check('grade >= 2.0 and grade <= 7.0');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('grades');
+    }
+}
